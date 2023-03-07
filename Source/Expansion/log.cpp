7f984739c6fac1a73767d2287e75c5e60ca1c4fa
ZhongCH <92720howard@gmail.com>
2023-03-07 16:44:21 +0800

    add comment in 'log.h' and 'log.cpp'

--- Source/Expansion/log.cpp
+++ Source/Expansion/log.cpp
@@ -10,16 +10,20 @@ std::ofstream logFile;
 void logInit() {
 	struct stat logFileInfo;
 
+	// 如果有舊的 log 檔案，則刪除
 	if (stat(LOG_FILENAME, &logFileInfo) == 0) {
 		remove(LOG_FILENAME);
 	}
 }
 
 void logMessage(const char *typeMessage, const char *message) {
+	// 以在最後加入文字的模式，開啟檔案
 	logFile.open(LOG_FILENAME, std::ios_base::app);
 
+	// 輸出訊息
 	logFile << typeMessage << message << '\n';
 
+	// 關閉檔案
 	logFile.close();
 }
 
--- Source/Expansion/log.h
+++ Source/Expansion/log.h
@@ -1,7 +1,16 @@
 #pragma once
 
+/*
+初始化 log
+*/
 void logInit();
 
+/*
+輸出 log 訊息：
+	info 一般訊息
+	warning 警告訊息
+	error 錯誤訊息
+*/
 void logInfo(const char *message);
 void logWarning(const char *message);
 void logError(const char *message);

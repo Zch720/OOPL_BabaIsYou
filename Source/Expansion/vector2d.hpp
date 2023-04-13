0708198603a3f3aaa3668ef3d3f068ef3e1c4678
ZhongCH <92720howard@gmail.com>
2023-04-13 18:36:43 +0800

    Fixed bug
    Fixed: vector2d 'foreach' can't work.

--- Source/Expansion/vector2d.hpp
+++ Source/Expansion/vector2d.hpp
@@ -6,7 +6,7 @@
 template<typename T>
 class vector2d : public std::vector<std::vector<T>> {
 public:
-	typedef std::function(void(T&)) ElemenetProcFunc;
+	typedef std::function<void(T&)> ElementProcFunc;
 
 	std::vector<T>& operator[](const int index) {
 		return this->_Myfirst()[index];
@@ -15,9 +15,9 @@ public:
 		return this->_Myfirst()[position.x][position.y];
 	}
 	void foreach(ElementProcFunc func) {
-		for (std::vector<T> &vec : this) {
+		for (std::vector<T> &vec : *this) {
 			for (T &element : vec) {
-				func(vec);
+				func(element);
 			}
 		}
 	}
